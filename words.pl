86fe093441c9f7c4e30ffbdf81f0510314e25fa1
jperas243 <joaop.pereira@sapo.pt>
2021-01-22 15:57:08 +0000

    save

--- words.pl
+++ words.pl
@@ -23,6 +23,8 @@ bin(B) :- cod(_,B).
 
 ambiguo(List, _, _, _) :- \+ compr(List, 0),bdMount(List). 
 
+same([],[]).
+same([H1|T1],[H2|T2]) :- H1=H2,T1=T2,same(T1,T2).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -30,11 +32,13 @@ ambiguo(List, _, _, _) :- \+ compr(List, 0),bdMount(List).
 
 
 wordBin(0, []).
-wordBin(N, [C|W]) :-
+wordBin(N, [C|W],[B1|B2]) :-
     N > 0,
     N1 is N-1,
     cod(C,_),
     wordBin(N1,W).
+
+
 
 
 deCode(A,R) :- deCode(A,[],R).
@@ -60,10 +64,49 @@ wordList(S,R) :-  findall(X,wordBin(S,X),R).
 
 
 
+
+
+binariesOf(S,R) :- wordsLessThan(1,Words),binariesOf(S,Words,[],R).
+binariesOf([],List,List).
+
+
+binariesOf(S,[H1|T1],[],R) :-
+
+        deCode(H1,B1),
+        compr(B1,S).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 subListOf(List,M) :- findall(X,sublista(X,List),M).
 
 
-%codeTo(Words,Bin,R) :- codeTo(Words,Bin,[],R)
-%codeTo([],[],Words).
+codeToList(Words,Bin,R) :- subListOf(Bin,SubBins),codeTo(Words,SubBins,[],R).
 
-codeTo(Bin,R) :- subListOf(Bin,M), member(SubBin,M), findall(L,cod(L,SubBin),R).
+codeTo([],_,List,List).
+codeTo([H1,T1],SubBins,List,R) :- 
+        
+        deCode(H1,B1),
+        member(B1,SubBins),
+        append(R1,H1,R),
+        codeTo(T1,SubBins,List,R1).
+
+giveMeTheSauce(S,R) :- wordsLessThan(S,Words),codeToList(Words,[0,1],R).
+
+%codeTo(Bin,L) :- subListOf(Bin,M), member(SubBin,M), cod(L,SubBin).
